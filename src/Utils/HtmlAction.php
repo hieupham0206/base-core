81250c05c3cbeec7d28a4a95a317f8be517c52b9
Phạm Quang Hiếu <hieu.pham@cloudteam.vn>
2020-03-31 16:49:46 +0700

    refactor: them chuc nang tao button dropdown, add custom class

--- src/Utils/HtmlAction.php
+++ src/Utils/HtmlAction.php
@@ -8,27 +8,40 @@ namespace Cloudteam\BaseCore\Utils;
 
 class HtmlAction
 {
-    public static function generateButtonChangeState($params)
+    private static function createCustomButtonClass($btnClass = '')
     {
+        return $btnClass ?: "btn-action $btnClass";
+    }
+
+    public static function generateButtonChangeState($params, $btnClass = '')
+    {
+        $btnClass = self::createCustomButtonClass($btnClass);
+
         [$state, $message, $title, $url, $elementTitle, $icon] = $params;
 
-        return sprintf(' <button type="button" class="btn-action-change-state" data-state="%s" data-message="%s" data-title="%s" data-url="%s" title="%s"><i class="%s"></i></button>',
-            $state, $message, $title, $url, $elementTitle, $icon);
+        return sprintf(' <button type="button" class="btn-action-change-state %s" data-state="%s" data-message="%s" data-title="%s" data-url="%s" title="%s"><i class="%s"></i></button>',
+            $btnClass, $state, $message, $title, $url, $elementTitle, $icon);
     }
 
-    public static function generateButtonDelete($deleteLink, $dataTitle)
+    public static function generateButtonDelete($deleteLink, $dataTitle, $btnClass = '')
     {
-        return sprintf(" <button type='button' class='btn-action-delete' data-title='%s' data-url='%s' title='%s'><i class='far fa-trash'></i></button>", $dataTitle, $deleteLink, __('Delete'));
+        $btnClass = self::createCustomButtonClass($btnClass);
+
+        return sprintf(" <button type='button' class='btn-action-delete %s' data-title='%s' data-url='%s' title='%s'><i class='far fa-trash'></i></button>", $btnClass, $dataTitle, $deleteLink, __('Delete'));
     }
 
-    public static function generateButtonEdit($editLink)
+    public static function generateButtonEdit($editLink, $btnClass = '')
     {
-        return sprintf(" <a href='%s' class='btn-action-edit' title='%s'><i class='far fa-edit'></i></a>", $editLink, __('Edit'));
+        $btnClass = self::createCustomButtonClass($btnClass);
+
+        return sprintf(" <a href='%s' class='btn-action-edit %s' title='%s'><i class='far fa-edit'></i></a>", $editLink, $btnClass, __('Edit'));
     }
 
-    public static function generateButtonView($viewLink)
+    public static function generateButtonView($viewLink, $btnClass = '')
     {
-        return sprintf(' <a href="%s" class="btn-action-view" title="%s"><i class="far fa-eye"></i></a>', $viewLink, __('View'));
+        $btnClass = self::createCustomButtonClass($btnClass);
+
+        return sprintf(' <a href="%s" class="btn-action-view %s" title="%s"><i class="far fa-eye"></i></a>', $viewLink, $btnClass, __('View'));
     }
 
     public static function generateCustomButton($params)
@@ -37,5 +50,17 @@ class HtmlAction
 
         return sprintf(' <button type="button" class="btn-action %s" data-title="%s" data-url="%s" title="%s"><i class="%s"></i></button>'
             , $cssClass, $dataTitle, $link, $title, $icon);
+    }
+
+    public static function generateDropdownButton($buttons, $btnClass = 'btn-gray')
+    {
+        $buttonHtml = implode(' ', $buttons);
+
+        return " <div class=\"dropdown dropdown-inline\">
+                            <button type=\"button\" class=\"btn-action $btnClass\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                                <i class=\"far fa-ellipsis-h\"></i>
+                            </button>
+                               <div class=\"form-group dropdown-menu dropdown-menu-right row text-center\">$buttonHtml</div>
+                        </div>";
     }
 }
